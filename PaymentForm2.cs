3647c3211ea49b2d51e3d1eb046812d700349ec8
ArezooTorabi <131034518+ArezooTorabi@users.noreply.github.com>
2023-04-23 14:10:21 +0430

    Initial commit

---
+++ PaymentForm2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multiform
+{
+    public partial class PaymentForm2 : Form
+    {
+        public PaymentForm pf;
+        SearchForm sf = new SearchForm();
+        public PaymentForm2()
+        {
+            InitializeComponent();
+            sf.pf2 = this;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sf.Show();
+        }
+
+        private void PaymentForm2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pf.listbox1.item.add(textBox1);
+        }
+
+
+    }
+}
---
+++ SearchForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multiform
+{
+    public partial class SearchForm : Form
+    {
+        public PaymentForm2 pf2;
+        public SearchForm()
+        {
+            InitializeComponent();
+        }
+        private void SearchForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( textBox1.Text == "Arezoo")
+            {
+              pf2.textbox2.text = "140112028037";
+                this.Hide();
+            } 
+        }
+
+    }
+}
